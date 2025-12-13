7e02c12a3fc81e3b6eb70253ffbfaa9fd228334b
Chow <chouuu0976286138@gmail.com>
2025-12-13 11:32:00 +0800

    Add app folder with routers

---
+++ app/routers/attraction.py
@@ -0,0 +1,106 @@
+from fastapi import APIRouter
+from app.db_connect import get_connection
+
+router = APIRouter()
+
+PAGE_SIZE = 8
+
+@router.get("/api/attractions")
+def get_attractions(page: int=0, keyword: str=None):
+    start = page * PAGE_SIZE
+
+    conn = get_connection()
+    cursor= conn.cursor(dictionary=True)
+
+    # 搜尋條件
+    if keyword:
+        query = """
+            SELECT * FROM attraction WHERE name LIKE %s OR mrt LIKE %s OR category LIKE %s LIMIT %s OFFSET %s
+        """
+        cursor.execute(query, (f"%{keyword}%", f"%{keyword}%", f"%{keyword}%", PAGE_SIZE +1, start))
+    else:
+        query = """
+            SELECT * FROM attraction LIMIT %s OFFSET %s
+        """
+        cursor.execute(query, (PAGE_SIZE +1, start))
+    
+    rows = cursor.fetchall()
+
+    # 判斷如果超過8筆，是否到下一頁
+    if len(rows)>PAGE_SIZE:
+        next_page = page+1
+        rows = rows[:PAGE_SIZE]
+    else:
+        next_page = None
+
+    # 取出 Attraction IDs 用於查詢圖片
+    attraction_ids = [row["id"] for row in rows]
+    if attraction_ids:
+        cursor.execute(
+            "SELECT attraction_id, url FROM image WHERE attraction_id IN (%s)" % ",".join (["%s"] * len(attraction_ids)),
+            attraction_ids
+        )
+        image_rows = cursor.fetchall()
+
+        images_map = {}
+        for img in image_rows:
+            images_map.setdefault(img["attraction_id"], []).append(img["url"])
+    else:
+        images_map = {}
+
+    # 組回傳資料
+    data = []
+    for row in rows:
+        data.append({
+            "id": row["id"],
+            "name": row["name"],
+            "category": row["category"],
+            "description": row["description"],
+            "address": row["address"],
+            "transport": row["transport"],
+            "mrt": row["mrt"],
+            "latitude": row["latitude"],
+            "longitude": row["longitude"],
+            "images": images_map.get(row["id"], [])
+        })
+
+    cursor.close()
+    conn.close()
+
+    return{"nextPage": next_page, "data":data}
+
+# /api/attraction/{id}
+@router.get("/api/attraction/{attraction_id}")
+def get_attraction(attraction_id: int):
+    
+    conn= get_connection()
+    cursor= conn.cursor(dictionary=True)
+
+    # 查景點
+    cursor.execute("SELECT * FROM attraction WHERE id= %s", (attraction_id,))
+    row = cursor.fetchone()
+
+    if not row:
+        return{"error": True, "message": "景點編號不正確"}
+    
+    # 查圖片
+    cursor.execute("SELECT url FROM image WHERE attraction_id = %s", (attraction_id,))
+    images = [img["url"] for img in cursor.fetchall()]
+
+    cursor.close()
+    conn.close()
+
+    return{
+        "data":{
+            "id": row["id"],
+            "name": row["name"],
+            "category": row["category"],
+            "description": row["description"],
+            "address": row["address"],
+            "transport": row["transport"],
+            "mrt": row["mrt"],
+            "latitude": row["latitude"],
+            "longitude": row["longitude"],
+            "images": images
+        }
+    }
