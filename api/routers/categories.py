5789c6015b80d868b651b7656aee3ec997ea00ab
Chow <chouuu0976286138@gmail.com>
2025-12-22 16:34:18 +0800

    Fix api file

---
+++ api/routers/categories.py
@@ -0,0 +1,20 @@
+
+from fastapi import APIRouter
+from api.db_connect import get_connection
+
+router = APIRouter()
+
+@router.get("/api/categories")
+def get_categories():
+    conn= get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT DISTINCT category FROM attraction ORDER BY category")
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    categories = [row[0] for row in rows]
+
+    return {"data": categories}
