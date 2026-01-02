e54dfd752afa9f9b41a1c2828c7d465d71944350
Chow <chouuu0976286138@gmail.com>
2026-01-02 16:19:45 +0800

    Fix user route

--- api/routers/user.py
+++ api/routers/user.py
@@ -23,28 +23,31 @@ def signup(user: UserSignUp):
     cursor = db.cursor(dictionary=True)
 
     # 檢查email是否重複
-    cursor.execute(
-        "SELECT id FROM user WHERE email = %s",(user.email,)
-    )
-    if cursor.fetchone():
-        return {
-            "error": True,
-            "message": "Email已經註冊帳戶"
-        }
-    
-    # 密碼加密hash
-    hashed_pw = bcrypt.hashpw(
-        user.password.encode("utf-8"),
-        bcrypt.gensalt()
-    )
-    cursor.execute(
-        "INSERT INTO user (name, email, password) VALUES (%s, %s, %s)", (user.name, user.email, hashed_pw.decode("utf-8"))
-    )
-    db.commit()
-    cursor.close()
-    db.close()
+    try:
+        cursor.execute(
+            "SELECT id FROM user WHERE email = %s",(user.email,)
+        )
+        if cursor.fetchone():
+            return {
+                "error": True,
+                "message": "Email已經註冊帳戶"
+            }
+        
+        # 密碼加密hash
+        hashed_pw = bcrypt.hashpw(
+            user.password.encode("utf-8"),
+            bcrypt.gensalt()
+        )
+        cursor.execute(
+            "INSERT INTO user (name, email, password) VALUES (%s, %s, %s)", (user.name, user.email, hashed_pw.decode("utf-8"))
+        )
+        db.commit()
 
-    return {"ok": True}
+        return {"ok":True}
+    
+    finally:
+        cursor.close()
+        db.close()
 
 
 # pyjwt--取得當前登入會員資訊設定
@@ -53,35 +56,37 @@ def signin(user: UserSignIn):
     db = get_connection()
     cursor = db.cursor(dictionary=True)
 
-    cursor.execute(
-        "SELECT id, name, email, password FROM user WHERE email=%s", (user.email,)
-    )
-    result = cursor.fetchone()
+    try:
+        cursor.execute(
+            "SELECT id, name, email, password FROM user WHERE email=%s", (user.email,)
+        )
+        result = cursor.fetchone()
 
-    if not result:
+        if not result:
+            cursor.close()
+            db.close()
+            return {
+                "error":True,
+                "message":"電子郵件或密碼錯誤"
+            }
+        if not bcrypt.checkpw(
+            user.password.encode("utf-8"),
+            result["password"].encode("utf-8")
+        ):
+            cursor.close()
+            db.close()
+            return {
+                "error": True,
+                "message": "電子郵件或密碼錯誤"
+            }
+        token = create_jwt({
+            "id": result["id"],
+            "name": result["name"],
+            "email": result["email"]
+        })
+
+        return {"token": token}
+    
+    finally:
         cursor.close()
         db.close()
-        return {
-            "error":True,
-            "message":"電子郵件或密碼錯誤"
-        }
-    if not bcrypt.checkpw(
-        user.password.encode("utf-8"),
-        result["password"].encode("utf-8")
-    ):
-        cursor.close()
-        db.close()
-        return {
-            "error": True,
-            "message": "電子郵件或密碼錯誤"
-        }
-    token = create_jwt({
-        "id": result["id"],
-        "name": result["name"],
-        "email": result["email"]
-    })
-
-    cursor.close()
-    db.close()
-
-    return {"token": token}
