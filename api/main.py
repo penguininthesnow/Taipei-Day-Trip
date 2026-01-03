d5478137fb3e4e2753a670e22d231e4d1aa99b1c
Chow <chouuu0976286138@gmail.com>
2026-01-04 01:04:46 +0800

    Fix main.py of connect api

--- api/main.py
+++ api/main.py
@@ -1,9 +1,19 @@
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+import os
 from api.routers import attraction, mrt, categories,user
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# 首頁
+@app.get("/")
+def read_index():
+    return FileResponse(os.path.join("static", "index.html"))
 
 app.include_router(attraction.router)
 app.include_router(mrt.router)
